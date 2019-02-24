884fc66cd657d51e573fc408429fadeaa0eede34
Ludovic DEDIER <ldedier@e2r2p19.42.fr>
2019-02-24 16:58:50 +0100

    fixed dlstpop_ functions

--- libft/srcs/ft_dlstpop.c
+++ libft/srcs/ft_dlstpop.c
@@ -6,7 +6,7 @@
 /*   By: ldedier <ldedier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/10 20:11:41 by ldedier           #+#    #+#             */
-/*   Updated: 2019/02/10 21:24:53 by ldedier          ###   ########.fr       */
+/*   Updated: 2019/02/24 16:57:36 by ldedier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,22 @@
 
 void	ft_dlstpop(t_dlist **list)
 {
-	void	*content;
-	t_dlist	*ptr;
+	void    *content;
+	t_dlist *ptr;
+	t_dlist *next;
 
 	if (*list != NULL)
 	{
 		ptr = *list;
+		next = (*list)->next;
 		content = ptr->content;
 		(*list)->prev->next = (*list)->next;
 		(*list)->next->prev = (*list)->prev;
 		free(content);
 		free(ptr);
-		if (*list == (*list)->next)
+		if (*list == next)
 			*list = NULL;
 		else
-			*list = (*list)->next;
+			*list = next;
 	}
 }
--- libft/srcs/ft_dlstpop_ptr.c
+++ libft/srcs/ft_dlstpop_ptr.c
@@ -6,7 +6,7 @@
 /*   By: ldedier <ldedier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/10 21:25:43 by ldedier           #+#    #+#             */
-/*   Updated: 2019/02/10 21:27:38 by ldedier          ###   ########.fr       */
+/*   Updated: 2019/02/24 16:57:38 by ldedier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,22 @@
 
 void	*ft_dlstpop_ptr(t_dlist **list)
 {
-	void	*content;
-	t_dlist	*ptr;
+	void    *content;
+	t_dlist *ptr;
+	t_dlist *next;
 
 	if (*list != NULL)
 	{
 		ptr = *list;
+		next = (*list)->next;
 		content = ptr->content;
 		(*list)->prev->next = (*list)->next;
 		(*list)->next->prev = (*list)->prev;
 		free(ptr);
-		if (*list == (*list)->next)
+		if (*list == next)
 			*list = NULL;
 		else
-			*list = (*list)->next;
+			*list = next;
 		return (content);
 	}
 	return (NULL);
